62caf5d47706f00b813db09eab599922aaf33106
Mat Dave Jones <mat@matdave.com>
2015-02-27 17:23:35 -0600

    Update hybridauth.class.php

--- core/components/hybridauth/model/hybridauth/hybridauth.class.php
+++ core/components/hybridauth/model/hybridauth/hybridauth.class.php
@@ -567,4 +567,28 @@ class HybridAuth {
 
 		return $result;
 	}
+	
+	
+	/**
+	* Get a local configuration option or a namespaced system setting by key.
+	*
+	* @param string $key The option key to search for.
+	* @param array $options An array of options that override local options.
+	* @param mixed $default The default value returned if the option is not found locally or as a
+	* namespaced system setting; by default this value is null.
+	* @return mixed The option value or the default value specified.
+	*/
+	public function getOption($key, $options = array(), $default = null) {
+		$option = $default;
+		if (!empty($key) && is_string($key)) {
+		    if ($options != null && array_key_exists($key, $options)) {
+		        $option = $options[$key];
+		    } elseif (array_key_exists($key, $this->options)) {
+		        $option = $this->options[$key];
+		    } elseif (array_key_exists("{$this->namespace}.{$key}", $this->modx->config)) {
+		        $option = $this->modx->getOption("{$this->namespace}.{$key}");
+		    }
+		}
+		return $option;
+	}
 }
